415ca3b5b9535c996ea4c7b5ebbfffda8653e209
aum99 <aumbattul99@gmail.com>
2023-11-05 02:51:28 +0530

    add billboard to homepage

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,9 +1,23 @@
 import { FC } from "react";
 
+import getBillboard from "@/actions/getBillboard";
+
+import Container from "@/components/layouts/Container";
+import Billboard from "@/components/Billboard";
+
 interface pageProps {}
 
-const HomePage: FC<pageProps> = ({}) => {
-  return <div>Hello home!</div>;
+export const revalidate = 0;
+
+const HomePage: FC<pageProps> = async ({}) => {
+  const billboard = await getBillboard("28f900d2-0e6c-434f-8128-e4cecf903125");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
